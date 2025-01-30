21ca161bd5bc5bc8fdb9b7f10ad7972464b414c3
Matias Gonzalez Perez <161659100+matogperez@users.noreply.github.com>
2025-01-30 10:25:20 -0600

    Update racionales.c

--- racionales.c
+++ racionales.c
@@ -4,13 +4,13 @@ typedef struct racional{
         int num;
         int den;
     }racional;
-
+int maximo_comun_divisor_recursivo(int a, int b);
 racional funcMult(racional r1,racional r2);
 racional funcResta(racional r1, racional r2);
 racional funcSuma(racional r1, racional r2);
 
 int main (void){
-    int opcion;
+    int opcion, mcd;
     
     racional n1, n2, nr;
     printf("\nDame el numerador de un número racional n1: ");
@@ -38,8 +38,12 @@ int main (void){
         nr=funcResta(n1,n2);
     else if (opcion ==1)
         nr=funcSuma(n1,n2);
+    mcd=maximo_comun_divisor_recursivo(nr.num,nr.den);
+    nr.num=nr.num/mcd;
+    nr.den=nr.den/mcd;
     printf("El resultado de la operación es:\n%i\n__\n%i\n", nr.num,nr.den);
 }
+
 
 racional funcMult(racional r1,racional r2){
     racional rf;
@@ -60,4 +64,9 @@ racional funcSuma(racional r1, racional r2){
     rf.num=(r1.num*r2.den)+(r2.num*r1.den);
     rf.den=(r1.den*r2.den);
     return rf;
+}
+
+int maximo_comun_divisor_recursivo(int a, int b) {
+    if (b == 0) return a;
+    return maximo_comun_divisor_recursivo(b, a % b);
 }
